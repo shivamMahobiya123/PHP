9a47d186ace28bd9959ecd7b3eb9341ed05c8ec1
Shivam Mahobiya <48170409+shivamMahobiya123@users.noreply.github.com>
2020-12-22 22:11:04 +0530

    file
    Use of print and echo element with using different ways of display data  ,variable ,data types etc

--- file1.php
+++ file1.php
@@ -1,1 +1,14 @@
+<?php
+// We can use for comment
+# "# sign for comment"
+// 
+$variable= "50"; 
+echo $variable;
 
+//multiple data types are given below
+$int =10;
+$float =10.1;
+$char=S;
+$string="Hello world"  ;
+print($char);
+?>
